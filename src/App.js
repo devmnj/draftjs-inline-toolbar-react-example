5a7efc143ce6e9b447a90f8e2bedfeebfd92932a
Manoj AP <manojap.nair@gmail.com>
2022-05-28 21:55:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Editor from "./Editor";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hi I'am a Draft-js Plugin</h1>
+      <Editor />
+    </div>
+  );
+}
